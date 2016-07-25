25223d1a9702505a3a3b463df7b00d8edeb8882b
Fabrice Daugan <fabrice.daugan@gmail.com>
2016-07-25 18:25:33 +0200

    Update submodules version

--- src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
+++ src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
@@ -500,7 +500,7 @@ public abstract class AbstractGitFlowMojo extends AbstractMojo {
                     + version, "-Dtycho.mode=maven");
         } else {
             executeMvnCommand(VERSIONS_MAVEN_PLUGIN_SET_GOAL, "--batch-mode", "-DdevelopmentVersion="
-                    + version + ".temp", "-DautoVersionSubmodules=true");
+                    + "5.0.0" + ".temp", "-DautoVersionSubmodules=true");
             getLog().info("Running sed...");
             // executeCommand(sedMvn, true, ".", "-type", "f", "-name", "pom.xml", "-exec", "sed", "-i", "s/" + version + ".temp-SNAPSHOT/" + version + "/g", "{}", "+");
         }
